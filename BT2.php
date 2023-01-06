35f51ff0ba0c9e11e589abfe7c409de1b4502d7e
Mai Kieu Anh Phan <kieuanh7101@gmail.com>
2023-01-06 23:08:35 +0700

    fixed-BT2

--- BT2.php
+++ BT2.php
@@ -8,100 +8,75 @@ class Student{
         $this->pointPhy = $pointPhy;
         $this->pointChem = $pointChem;
     } 
-    public function layDuLieuHocSinh(){
+    public function xuatThongTinHocSinh(){
         echo implode(" ", [
             $this->name,
             $this->year,
-            $this->pointMath,
-            $this->pointPhy,
-            $this->pointChem,
+            $this->getAverage(),
         ]);
     }
-}
-class Average extends Student{
-    protected static $names, $years, $pointMaths, $pointChems, $pointPhys;
-    public static function setAverage($name, $year, $pointChem, $pointMath, $pointPhy){
-        self::$pointChems = $pointChem;
-        self::$pointMaths = $pointMath;
-        self::$pointPhys = $pointPhy;
-        self::$names = $name;
-        self::$years = $year;
+    public function getAverage(){
+        return number_format((($this->pointChem + $this->pointPhy + $this->pointMath) / 3), 2);
         
-        return number_format((($pointChem + $pointPhy + $pointMath) / 3), 2);
     }
-    public static function getAverage($name, $year, $pointChem, $pointMath, $pointPhy){
-        echo implode(" ", [
-            "Diem trung binh ",
-            $name, 
-            $year, 
-            Average::setAverage($name, $year, $pointChem, $pointMath, $pointPhy),
-        ]);
-    }
-    
-    public static function getAverage5($name, $year, $pointChem, $pointMath, $pointPhy){
-        $average = Average::setAverage($name, $year, $pointChem, $pointMath, $pointPhy);
-        if($average >= 5 ){
-            echo implode(" ", [
-                "Trung binh lon hon 5",
-                $name, 
-                $average,
-            ]);
-        }
-    }
-}
-class Year extends Student{
     public function getYear()
     {     
-        if($this->year == 2001){
-            echo implode(" ",[ 
-                $this->name,
-                $this->year,
-            ]);
-        }
-        return $this;
+        return $this->year;
     }
 }
-
-class ListStudent{
-    public static function getList(){
-        return [
+class QuanLyHocSinh{
+    public function layDuLieuHocSinh(){
+        $du_lieu_HS = [
             ["A", 2001, 7, 8,9],
             ["B", 2002, 5, 4,3],
             ["C", 2001, 10, 10,9],
             ["D", 2002, 9, 8,9],
             ["E", 2000, 4, 5,5],
         ];
+        $listHocSinh = [];
+        foreach($du_lieu_HS as $student){
+            list($name, $year, $pointMath, $pointPhy, $pointChem) = $student;
+            $listHocSinh[] = new Student($name, $year, $pointMath, $pointPhy, $pointChem);
+        }
+        return $listHocSinh;
+    }
+    public function xuatDanhSachHS($listHocSinh){
+        foreach($listHocSinh as $item){
+            $item->xuatThongTinHocSinh();
+        }
+    }
+    
+    public static function xuatTBLonHon5($listHocSinh){
+        foreach($listHocSinh as $item){
+            if($item->getAverage() > 5){
+                $item->xuatThongTinHocSinh();
+            }
+        }
+    }
+    public function xuatHSNam2001($listHocSinh){
+        foreach($listHocSinh as $item){
+            if($item->getyear() == 2001){
+                $item->xuatThongTinHocSinh();
+            }
+        }
     }
 }
 
 class Main{
     public static function run()
     {
-        $list = [];
-        foreach (ListStudent::getList() as $student) {
-            list($name, $year, $pointMath, $pointPhy, $pointChem) = $student;
-            
-            $list_average = new Average($name, $year, $pointMath, $pointPhy, $pointChem);
-            echo "<br/>";
-            echo "<br/>";
-            $list_average->getAverage($name, $year, $pointMath, $pointPhy, $pointChem);
-            // Lay danh sach trung binh
-            $list_average5 = new Average($name, $year, $pointMath, $pointPhy, $pointChem);
-            echo "<br/>";
-            // danh sach diem lon hon 5
-            $list_average5->getAverage5($name, $year, $pointMath, $pointPhy, $pointChem);
-            
-            // danh sach sinh nam 2001
-            $list[] = new Year($name, $year, $pointMath, $pointPhy, $pointChem);
-        }
-        
-        echo "<br/>";
-        echo "Sinh nam 2001";
-        foreach($list as $item){
-            echo "<br/>";
-            $item->getYear();
-        }
+        $truong = new QuanLyHocSinh();
+        $listHocSinh = $truong->layDuLieuHocSinh();
 
+        echo "Danh sach diem trung binh: <br>";
+        $truong->xuatDanhSachHS($listHocSinh);        
+        
+        echo "<br>Danh sach diem trung binh lon hon 5: <br>" ;
+
+        $truong->xuatTBLonHon5($listHocSinh);     
+
+        echo "<br>Danh sach diem trung binh: <br>";
+        $truong->xuatHSNam2001($listHocSinh);     
     }
 }
 Main::run();
