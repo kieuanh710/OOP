68fc697bc6c0c067487a57d29bd7d0c2eb59a346
Mai Kieu Anh Phan <kieuanh7101@gmail.com>
2023-01-09 22:48:19 +0700

    sua BT3

--- BT3.php
+++ BT3.php
@@ -1,12 +1,10 @@
-<?php
-use SebastianBergmann\CodeCoverage\Report\PHP;
-class NhanVien{
-    protected $ten, $nam_sinh, $chuc_vu, $luong_co_ban, $nam_kinh_nghiem, $doanh_so;
-    public function __construct($ten, $nam_sinh, $chuc_vu, $doanh_so, $nam_kinh_nghiem, $luong_co_ban){
+<?php 
+abstract class NhanVien{
+    protected $ten, $nam_sinh, $chuc_vu, $luong_co_ban, $nam_kinh_nghiem;
+    public function __construct($ten, $nam_sinh, $chuc_vu, $nam_kinh_nghiem, $luong_co_ban){
         $this->ten = $ten;
         $this->nam_sinh = $nam_sinh;
         $this->chuc_vu = $chuc_vu;
-        $this->doanh_so = $doanh_so;
         $this->nam_kinh_nghiem = $nam_kinh_nghiem;
         $this->luong_co_ban = $luong_co_ban;
     } 
@@ -18,44 +16,72 @@ class NhanVien{
             $this->getLuong(),
         ]);
     }
+    abstract public function getLuong();
+} 
+class LapTrinh extends NhanVien{
     public function getLuong(){
-        if($this->chuc_vu == "LapTrinh"){
-            return $this->luong_co_ban * 150 + $this->luong_co_ban * (0.5 * $this->nam_kinh_nghiem);
-        }
-        else if($this->chuc_vu == "BanHang"){
-            return $this->luong_co_ban * 30 + $this->doanh_so * 15;
-        }
-        else if($this->chuc_vu == "GiamDoc"){
-            return $this->luong_co_ban * $this->nam_kinh_nghiem;
-        }
-        else if($this->chuc_vu == "LeTan"){
-            return $this->luong_co_ban * 30 + $this->luong_co_ban * (0.2 * $this->nam_kinh_nghiem);
-        }    
+        return $this->luong_co_ban * 150 + $this->luong_co_ban * (0.5 * $this->nam_kinh_nghiem);
+    }
+} 
+class BanHang extends NhanVien{
+    private $doanh_so;
+    public function __construct($ten, $nam_sinh, $chuc_vu, $nam_kinh_nghiem, $luong_co_ban, $doanh_so){
+        parent::__construct($ten, $nam_sinh, $chuc_vu, $nam_kinh_nghiem, $luong_co_ban);
+        $this->doanh_so = $doanh_so;
     } 
+    public function getLuong(){
+        return $this->luong_co_ban * 30 + intval($this->doanh_so) * 15;
+    }
 }
+class LeTan extends NhanVien{
+    public function getLuong(){
+        return intval($this->luong_co_ban * 30 + $this->luong_co_ban * (0.2 * $this->nam_kinh_nghiem));
+    }
+}  
+class GiamDoc extends NhanVien{
+    public function getLuong(){
+        return $this->luong_co_ban * $this->nam_kinh_nghiem;
+    }
+}
+
 class QuanLyNhanVien{
     public function layDuLieuNhanVien(){
         $dataNV = [
-            ["Van A", 1997, "LeTan", 0, 1, 7000], 
-            ["Le B", 1995, "LeTan", 0, 3, 12000],
+            ["Van A", 1997, "LeTan", null, 1, 7000], 
+            ["Le B", 1995, "LeTan", null, 3, 12000],
             ["Ma C", 1999, "BanHang", 5, 1, 7500],
             ["Nguyen D", 1994, "BanHang", 12, 3, 15000],              
             ["Mai E", 1988, "BanHang", 23, 7, 25000],              
-            ["Tran F", 1993, "LapTrinh", 0, 5, 40000],              
-            ["Ly G", 2000, "LapTrinh", 0, 1, 8000],              
-            ["Pham H", 1997, "LapTrinh", 0, 2, 1500],              
+            ["Tran F", 1993, "LapTrinh", null, 5, 40000],              
+            ["Ly G", 2000, "LapTrinh", null, 1, 8000],              
+            ["Pham H", 1997, "LapTrinh", null, 2, 1500],              
             ["Hoang I", 1997, "BanHang", 8, 2, 100000],           
-            ["Trinh J", 1998, "LeTan", 0, 1, 7000],         
-            ["Dao K", 1985, "GiamDoc", 0, 7, 50000],
+            ["Trinh J", 1998, "LeTan", null, 1, 7000],         
+            ["Dao K", 1985, "GiamDoc", null, 7, 50000],
         ];
+        
         $listNhanVien = [];
         foreach($dataNV as $nhanvien){
             list($ten, $nam_sinh, $chuc_vu, $doanh_so, $nam_kinh_nghiem, $luong_co_ban) = $nhanvien;
-            $listNhanVien[] = new NhanVien($ten, $nam_sinh, $chuc_vu, $doanh_so, $nam_kinh_nghiem, $luong_co_ban);
+            if(($chuc_vu == "LapTrinh")){
+                $listNhanVien[] = new LapTrinh($ten, $nam_sinh, $chuc_vu, $nam_kinh_nghiem, $luong_co_ban);
+            } else if(($chuc_vu == "BanHang")){
+                $listNhanVien[] = new BanHang($ten, $nam_sinh, $chuc_vu, $nam_kinh_nghiem, $luong_co_ban, $doanh_so);
+            } else if(($chuc_vu == "LeTan")){
+                $listNhanVien[] = new LeTan($ten, $nam_sinh, $chuc_vu, $nam_kinh_nghiem, $luong_co_ban);
+            } else if(($chuc_vu == "GiamDoc")){
+                $listNhanVien[] = new GiamDoc($ten, $nam_sinh, $chuc_vu, $nam_kinh_nghiem, $luong_co_ban); 
+            }
         }
         return $listNhanVien;
     }
-    
+
+    // public function xuatThongTin($listNhanVien){
+    //     foreach ($listNhanVien as $item) {
+    //         $item->xuatThongTinNhanVien();
+    //         echo "<br>";
+    //     }  
+    // }
     public function tinhTongLuong($listNhanVien){
         $tongLuong = 0;
         foreach($listNhanVien as $item){
@@ -129,15 +155,14 @@ class QuanLyNhanVien{
         }
     }
 }
-
 class Main{
-    public static function run()
-    {
+    public static function run(){
         $congty = new QuanLyNhanVien();
         $listNhanVien = $congty->layDuLieuNhanVien();
+        // $congty->xuatThongTin($listNhanVien);     
         
         echo "Tong luong phai tra: <br>";  
-        $congty->xuatTongLuong($listNhanVien);     
+        $congty->xuatTongluong($listNhanVien);     
         
         echo "<br>Luong cao nhat: <br>";
         $congty->xuatLuongCaoNhat($listNhanVien);   
@@ -152,8 +177,9 @@ class Main{
         $congty->xuatNVLuongNhoHonTB($listNhanVien);     
         
         echo "<br>Danh sach nhan vien luong lon hon TB: <br>";
-        $congty->xuatNVLuongLonHonTB($listNhanVien);     
+        $congty->xuatNVLuongLonHonTB($listNhanVien);   
+
     }
 }
-Main::run();
+Main::run();   
 ?>
---
+++ BT4.php
@@ -0,0 +1,25 @@
+<?php 
+
+class QuickSort{
+  protected $high, $low, $left, $right;
+  public function partitison($arr){
+    if(count($arr) <= 1){
+      return $arr;
+    } else{
+      $pivot = $arr[0];
+      $left = array();
+      $right = array();
+      for ($i = 1; $i < count($arr); $i++){
+        
+      }
+      
+    }
+  }
+}
+class Main{
+    public static function run(){
+        
+    }
+}
+Main::run();   
+?>
